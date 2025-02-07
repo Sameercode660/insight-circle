c4bd01ab49a00529b810c0deb6fa07c2b183660c
Mohd Sameer <mesh789736@gmail.com>
2025-02-07 17:11:15 +0530

    chat event added

--- backend/server.ts
+++ backend/server.ts
@@ -3,10 +3,15 @@ import next from "next";
 import { Server } from "socket.io";
 import type { Server as HttpServer } from "http";
 import type { Socket } from "socket.io";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const dev: boolean = process.env.NODE_ENV !== "production";
 const hostname: string = "localhost";
 const port: number = 3000;
+
+const users = new Map<string, string>();
 
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
@@ -25,6 +30,42 @@ app.prepare().then(() => {
 
   io.on("connect", (socket: Socket) => {
     console.log("A client connected");
+
+    socket.on("join-user", (userId) => {
+      users.set(userId, socket.id);
+      console.log("user with id: ", userId, "joined");
+      socket.emit("user-joined", userId);
+    });
+
+    socket.on("send-message", async (reciverId, messageObject) => {
+      const reciver_socket_id = users.get(reciverId);
+      const sender_socket_id = users.get(messageObject.userId)
+      console.log(reciver_socket_id)
+      console.log(reciverId, messageObject)
+
+      // save message to db
+      const savedMessage = await prisma.message.create({
+        data: messageObject,
+      });
+      const sender = await prisma.user.findUnique({
+        where: {
+          id: messageObject.userId,
+        },
+      });
+      const reciver = await prisma.user.findUnique({
+        where: {
+          id: messageObject.mentorId,
+        },
+      });
+      console.log(users)
+      if (reciver_socket_id) {
+        io.to(reciver_socket_id).emit("receive-message", sender, reciver, savedMessage);
+      } else if (sender_socket_id) {
+        io.to(sender_socket_id).emit("receive-message", sender, reciver, messageObject);
+      } else {
+        console.error('Both sender_socket_id and reciver_socket_id are undefined.');
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("Client disconnected");
--- backend/src/app/api/user/fetch-mentor/route.ts
+++ backend/src/app/api/user/fetch-mentor/route.ts
@@ -23,6 +23,7 @@ export async function GET() {
             data: mentors
         })
     } catch (error) {
+        console.log(error)
         return NextResponse.json({
             statusCode: 500,
             message: 'Unable to run fetch subscriber',
